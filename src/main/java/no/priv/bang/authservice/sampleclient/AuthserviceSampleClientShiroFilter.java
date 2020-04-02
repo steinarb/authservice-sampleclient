7ff816efebf1a4764074efb6c6b182f9d7210ee8
Steinar Bang <sb@dod.no>
2020-04-02 23:17:28 +0200

    Replace use of deprecated Shiro classes, use the ini file to replace the authc filter with the PassThruAuthenticationFilter, which will work with nginx and authservice

--- src/main/java/no/priv/bang/authservice/sampleclient/AuthserviceSampleClientShiroFilter.java
+++ src/main/java/no/priv/bang/authservice/sampleclient/AuthserviceSampleClientShiroFilter.java
@@ -15,17 +15,12 @@
  */
 package no.priv.bang.authservice.sampleclient;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.Filter;
+
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.mgt.eis.SessionDAO;
-import org.apache.shiro.web.config.IniFilterChainResolverFactory;
-import org.apache.shiro.web.config.WebIniSecurityManagerFactory;
-import org.apache.shiro.web.filter.authc.PassThruAuthenticationFilter;
-import org.apache.shiro.web.filter.mgt.PathMatchingFilterChainResolver;
+import org.apache.shiro.web.env.IniWebEnvironment;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -41,7 +36,6 @@ import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
  *
  * The filter maps URLs in the webapp to users and roles.
  */
-@SuppressWarnings("deprecation")
 @Component(
     property= {
         HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN+"=/*",
@@ -72,24 +66,21 @@ public class AuthserviceSampleClientShiroFilter extends AbstractShiroFilter { //
 
     @Activate
     public void activate() {
-        WebIniSecurityManagerFactory securityManagerFactory = new WebIniSecurityManagerFactory(INI_FILE); // NOSONAR will replace this later
-        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) securityManagerFactory.createInstance();
+        IniWebEnvironment environment = new IniWebEnvironment();
+        environment.setIni(INI_FILE);
+        environment.setServletContext(getServletContext());
+        environment.init();
+
         DefaultWebSessionManager sessionmanager = new DefaultWebSessionManager();
         sessionmanager.setSessionDAO(session);
         sessionmanager.setSessionIdUrlRewritingEnabled(false);
-        securityManager.setSessionManager(sessionmanager);
-        setSecurityManager(securityManager);
-        securityManager.setRealm(realm);
-        // Using the PassThruAuthenticationFilter instead of the default authc FormAuthenticationFilter
-        // to be able to do a redirect back "out of" authservice to the originalUrl
-        Map<String, Object> defaultBeans = new HashMap<>(securityManagerFactory.getBeans());
-        PassThruAuthenticationFilter authc = new PassThruAuthenticationFilter();
-        authc.setLoginUrl("/login");
-        defaultBeans.put("authc", (Object)authc);
 
-        IniFilterChainResolverFactory filterChainResolverFactory = new IniFilterChainResolverFactory(INI_FILE, defaultBeans);
-        PathMatchingFilterChainResolver resolver = (PathMatchingFilterChainResolver) filterChainResolverFactory.createInstance();
-        setFilterChainResolver(resolver);
+        DefaultWebSecurityManager securityManager = DefaultWebSecurityManager.class.cast(environment.getWebSecurityManager());
+        securityManager.setSessionManager(sessionmanager);
+        securityManager.setRealm(realm);
+
+        setSecurityManager(securityManager);
+        setFilterChainResolver(environment.getFilterChainResolver());
     }
 
 }

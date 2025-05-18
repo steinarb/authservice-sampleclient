ce2c783522c87bc360bd6de066b66b2419ee6996
Steinar Bang <sb@dod.no>
2025-05-18 10:26:01 +0200

    Remove unnecessary same package import

--- src/test/java/no/priv/bang/authservice/sampleclient/AuthserviceSampleClientShiroFilterTest.java
+++ src/test/java/no/priv/bang/authservice/sampleclient/AuthserviceSampleClientShiroFilterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 Steinar Bang
+ * Copyright 2019-2025 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@ import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import no.priv.bang.authservice.sampleclient.AuthserviceSampleClientShiroFilter;
 
 class AuthserviceSampleClientShiroFilterTest extends ShiroTestBase {
 
